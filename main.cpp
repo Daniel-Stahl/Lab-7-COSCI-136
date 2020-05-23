7c27a33af4210966aa6d03556999712fd272ec97
Daniel Stahl <danny.stahl@gmail.com>
2020-05-23 10:36:16 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LAB_7_STAHL_DANIEL
+//
+//  Created by Daniel Stahl on 5/23/20.
+//  Copyright © 2020 Daniel Stahl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
